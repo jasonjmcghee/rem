b8f5497ee566ed34be39c1975f615eaae173c64c
Jason McGhee <mcghee.j@gmail.com>
2023-12-16 09:45:25 -0800

    Initial Commit

---
+++ rem/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  rem
+//
+//  Created by Jason McGhee on 12/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rem/remApp.swift
@@ -0,0 +1,17 @@
+//
+//  remApp.swift
+//  rem
+//
+//  Created by Jason McGhee on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct remApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
